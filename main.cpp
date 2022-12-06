fb9fe530229544eae804c835ab2ce815ce91128c
jacksonarich <98556905+jacksonarich@users.noreply.github.com>
2022-12-05 23:15:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,151 @@
 using namespace std;
 using Txr = sf::Texture;
 
+class LLRBBST {
+
+public:
+
+    LLRBBST() { root = nullptr; }
+
+    void insert(int thing) {
+        root = insert(root, thing);
+        root->color = false;
+    }
+
+    void print(vector<string> &words) {
+        string output = "digraph {\n    node [style=filled,fontcolor=white,fillcolor=black]\n";
+        if (!printIsSpecial(words, output)) {
+            bool hasReds = false;
+            generateDOTColors(root, words, output, hasReds);
+            if (hasReds) {
+                output.pop_back();
+                output += " [fillcolor=red]\n";
+            }
+            generateDOTFile(root, words,output);
+        }
+        output += "}\n";
+        cout << output;
+    }
+
+private:
+
+    typedef struct Node {
+        int thing, numDuplicates;
+        bool color;
+        struct Node *left, *right;
+    } Node;
+
+    Node *root;
+
+    static Node* createNode(int thing) {
+        Node *myNode = new Node();
+        myNode->thing = thing;
+        myNode->numDuplicates = 0;
+        myNode->color = true;
+        myNode->left = myNode->right = nullptr;
+        return myNode;
+    }
+
+    static Node* rotateLeft(Node* myNode) {
+        Node *child = myNode->right;
+        Node *childLeft = child->left;
+        child->left = myNode;
+        myNode->right = childLeft;
+        return child;
+    }
+
+    static Node* rotateRight(Node* myNode) {
+        Node *child = myNode->left;
+        Node *childRight =  child->right;
+        child->right = myNode;
+        myNode->left = childRight;
+        return child;
+    }
+
+    static int isRed(Node *myNode) {
+        return myNode == nullptr ? 0 : myNode->color;
+    }
+
+    static void swapColors(Node *node1, Node *node2) {
+        bool temp = node1->color;
+        node1->color = node2->color;
+        node2->color = temp;
+    }
+
+    Node* insert(Node* myNode, int thing) {
+        if (myNode == nullptr) return createNode(thing);
+        if (thing < myNode->thing) myNode->left = insert(myNode->left, thing);
+        else if (thing > myNode->thing) myNode->right = insert(myNode->right, thing);
+        else { myNode->numDuplicates++; return myNode; }
+
+        if (isRed(myNode->right) && !isRed(myNode->left)) {
+            myNode = rotateLeft(myNode);
+            swapColors(myNode, myNode->left);
+        }
+        if (isRed(myNode->left) && isRed(myNode->left->left)) {
+            myNode = rotateRight(myNode);
+            swapColors(myNode, myNode->right);
+        }
+        if (isRed(myNode->left) && isRed(myNode->right)) {
+            myNode->color = !myNode->color;
+            myNode->left->color = false;
+            myNode->right->color = false;
+        }
+        return myNode;
+    }
+
+    void generateDOTColors(Node* myNode, vector<string> &words, string &output, bool &hasReds) {
+        if (myNode->color) {
+            if (!hasReds) output += "    ";
+            hasReds = true;
+            output += words[myNode->thing] + ",";
+        }
+        bool noLeft = myNode->left == nullptr, noRight = myNode->right == nullptr;
+        if (noLeft) {
+            if (!noRight) {
+                generateDOTColors(myNode->right, words, output, hasReds);
+            }
+        } else {
+            if (noRight) {
+                generateDOTColors(myNode->left, words, output, hasReds);
+            } else {
+                generateDOTColors(myNode->left, words, output, hasReds);
+                generateDOTColors(myNode->right, words, output, hasReds);
+            }
+        }
+    }
+
+    bool printIsSpecial(vector<string> &words, string &output) {
+        if (root == nullptr) return true;
+        if (root->left == nullptr and root->right == nullptr) {
+            output += "    " + words[root->thing] + "\n";
+            return true;
+        }
+        return false;
+    }
+
+    void generateDOTFile(Node* myNode, vector<string> &words, string &output) {
+        bool noLeft = myNode->left == nullptr, noRight = myNode->right == nullptr;
+        if (noLeft and noRight) return;
+        output += "    " + words[myNode->thing] + " -> ";
+        if (noLeft) {
+            if (!noRight) {
+                output += words[myNode->right->thing] + "\n";
+                generateDOTFile(myNode->right, words, output);
+            }
+        } else {
+            if (noRight) {
+                output += words[myNode->left->thing] + "\n";
+                generateDOTFile(myNode->left, words, output);
+            } else {
+                output += "{" + words[myNode->left->thing] + ", " + words[myNode->right->thing] + "}\n";
+                generateDOTFile(myNode->left, words, output);
+                generateDOTFile(myNode->right, words, output);
+            }
+        }
+    }
+};
+
 class TextureStore {
 public:
     Txr boxTxr;
@@ -58,8 +203,15 @@ public:
     }
 };
 
-void setCurrentSlide(sf::Sprite &slideBG, int slideIndex, TextureStore &textureStore) {
+void setCurrentSlide(sf::Sprite &slideBG, int slideIndex, TextureStore &textureStore, bool &inTextbox, Textbox &textbox) {
     slideBG.setTexture(textureStore.slideTxrs[slideIndex]);
+    if (slideShow == 44) {
+        inTextbox = true;
+        textbox.setSelected(true);
+    } else {
+        inTextbox = false;
+        textbox.setSelected(false);
+    }
 }
 
 void setSprite(sf::Sprite &sprite, float xScale, float yScale, float pixelX, float pixelY) {
@@ -82,11 +234,15 @@ int main() {
 
     sf::Sprite slideBG, slideGif;
     int slideIndex = 0;
+    
+    Textbox textbox(sf::Color::Black, 50, true);
+    textbox.setFont(pixilFont);
+    textbox.setPosition(0, 0);
+    
+    bool inTextbox = false;
 
-    setCurrentSlide(slideBG, slideIndex, textureStore);
-    setSprite(slideGif, 72, 72, 128 - 72, 0);
-    slideGif.setTexture(textureStore.buttonTxrs[0]);
-
+    setCurrentSlide(slideBG, slideIndex, textureStore, isInTextbox, textbox);
+    
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -101,21 +257,21 @@ int main() {
                         sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
                         if (slideIndex != 45) {
                             slideIndex++;
-                            setCurrentSlide(slideBG, slideIndex, textureStore);
+                            setCurrentSlide(slideBG, slideIndex, textureStore, isInTextbox, textbox);
                         }
                     } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) ||
                                sf::Keyboard::isKeyPressed(sf::Keyboard::Up) ||
                                sf::Keyboard::isKeyPressed(sf::Keyboard::Backspace)) {
                         if (slideIndex != 0) {
                             slideIndex--;
-                            setCurrentSlide(slideBG, slideIndex, textureStore);
+                            setCurrentSlide(slideBG, slideIndex, textureStore, isInTextbox, textbox);
                         }
                     }
             }
         }
         window.clear(sf::Color(0, 0, 0, 255));
         window.draw(slideBG);
-        window.draw(slideGif);
+        textbox.drawTo(window);
         window.display();
     }
 }
